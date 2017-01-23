0ce9be9d7a49c5ca5b7fba7eb59b70911d3e76ee
Lohith Kini <lkini86@gmail.com>
2017-01-23 00:21:40 -0500

    ipynb in base

--- analysis/util.py
+++ analysis/util.py
@@ -10,19 +10,15 @@ import nibabel as nib
 import numpy as np
 from scipy.ndimage import morphology
 
-from Echobase.Common import errors
-from Echobase.Network.Metrics.globaltopo import synchronizability
-from Echobase.Network.Transforms import lesion
-
-with open('../data/DATA.json') as json_data_file:
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+with open(os.path.join(BASE_DIR,'data/DATA.json')) as json_data_file:
     data = json.load(json_data_file)
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-def get_resected_electrodes(patient_id, dilate_radius=0):
+def get_resected_electrodes(PATIENT_ID, dilate_radius=0):
     '''
     Utility function to compute and output labels of all resected electrodes.
-    :param patient_id: Patient ID (str)
+    :param PATIENT_ID: Patient ID (str)
     :param dilate_radius: Amount of dilation/erosion to apply to resection image.
     :return: list of tuples: (electrode IDs, electrode label)
     '''
@@ -32,16 +28,16 @@ def get_resected_electrodes(patient_id, dilate_radius=0):
 
     # Get resection image
     try:
-        resection_nii = nib.load(os.path.expanduser(data['PATIENTS'][patient_id]['RESECTION_IMAGE']))
+        resection_nii = nib.load(os.path.expanduser(data['PATIENTS'][PATIENT_ID]['RESECTION_IMAGE']))
         resection_data = resection_nii.get_data()
         # Threshold and binarize the resection mask
         resection_data[resection_data >= 0.8] = 1
         resection_data[resection_data < 0.8] = 0
         resection_affine = resection_nii.get_affine()
     except KeyError:
-        assert isinstance(patient_id, str)
+        assert isinstance(PATIENT_ID, str)
         print 'The PATIENT ID %s is not configured in the DATA JSON config file. ' \
-              'Please use a PATIENT ID that is included in the study.' % patient_id
+              'Please use a PATIENT ID that is included in the study.' % PATIENT_ID
         raise
 
     # Load electrode labels and generate electrode image
@@ -50,7 +46,7 @@ def get_resected_electrodes(patient_id, dilate_radius=0):
     radius = 2  # Radius of electrodes
     try:
         # Generate electrode image
-        lines = open(os.path.expanduser(data['PATIENTS'][patient_id]['ELECTRODE_LABELS']), 'r').readlines()
+        lines = open(os.path.expanduser(data['PATIENTS'][PATIENT_ID]['ELECTRODE_LABELS']), 'r').readlines()
         for line in lines:
             electrode_id = int(line.split(',')[0])
             X = int(float(line.split(',')[1]))
@@ -68,7 +64,7 @@ def get_resected_electrodes(patient_id, dilate_radius=0):
               'and make sure all coordinates are floating point values.'
         raise
     except KeyError:
-        print 'Make sure the data config file has the patient %s and their ELECTRODE_LABELS file path is set.' % patient_id
+        print 'Make sure the data config file has the patient %s and their ELECTRODE_LABELS file path is set.' % PATIENT_ID
         raise
 
     # Erode/Dilate the resection image appropriately
@@ -85,11 +81,10 @@ def get_resected_electrodes(patient_id, dilate_radius=0):
 
     return resected_electrodes
 
-def write_resected_electrodes(patient_id, dilate_radius=0):
+def write_resected_electrodes(PATIENT_ID, dilate_radius=0):
     '''
-    
     Utility function to write resected electrode labels to CSV.
-    :param patient_id: Patient ID (str)
+    :param PATIENT_ID: Patient ID (str)
     :param dilate_radius: Amount of dilation/erosion to apply to resection image.
     :return: None
     '''
@@ -102,46 +97,7 @@ def write_resected_electrodes(patient_id, dilate_radius=0):
     else:
         suffix = '0'
 
-    # Check if already created
-    comp_dir = data["COMP_DIR"]
-    if os.path.isfile(os.path.join(comp_dir, patient_id, 'aim1/%s_resected_electrodes_%s.csv' % (patient_id, suffix))):
-        return
-
     # Write to CSV file
-    open(os.path.join(comp_dir, patient_id, 'aim1/%s_resected_electrodes_%s.csv' % (patient_id, suffix)), 'w').write(
+    open(os.path.join(BASE_DIR,'data/%s_resected_electrodes_%s.csv' % (PATIENT_ID, suffix)), 'w').write(
         '\n'.join(map(lambda x: ','.join(x),
-                      get_resected_electrodes(patient_id, dilate_radius))))
-
-def region_control(adj, node_list):
-    """
-    Function for computing control centrality of a subregion, aka resection zone (change in synchronizability)
-    Parameters
-    ----------
-        adj: ndarray, shape (N, N)
-            Undirected, symmetric adjacency matrix with N nodes
-
-        node_list: list
-            List of node indices to simultaneously remove from the network
-    Returns
-    -------
-        control_vec: ndarray, shape (N,)
-            Control centrality based on delta_sync for each of N variates
-    """
-
-    # Standard param checks
-    errors.check_type(adj, np.ndarray)
-    errors.check_dims(adj, 2)
-    if not (adj == adj.T).all():
-        raise Exception('Adjacency matrix is not undirected and symmetric.')
-
-    # Get data attributes
-    n_node = adj.shape[0]
-
-    # Get the original synchronizability
-    base_sync = synchronizability(adj)
-
-    for i in xrange(n_node):
-        adj_lesion = lesion.node_lesion(adj, node_list)
-        lesion_sync = synchronizability(adj_lesion)
-
-    return (lesion_sync-base_sync) / base_sync
+                      get_resected_electrodes(PATIENT_ID, dilate_radius))))
