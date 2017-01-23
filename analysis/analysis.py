a929a4af5448b26006d8e301ddb04ab130226506
smartrama <lkini86@gmail.com>
2017-01-22 23:46:45 -0500

    modified data.json and created e01,e02

--- analysis/analysis.py
+++ analysis/analysis.py
@@ -8,14 +8,23 @@ from util import *
 
 if __name__ == '__main__':
     # Get arguments
-    PATIENT_ID = sys.argv[1]
     try:
+        patient_id = sys.argv[1]
         dilate_radius = int(sys.argv[2])
     except IndexError:
         dilate_radius = 0
     except TypeError:
-        print 'Please enter an integer for dilation/erosion radius.'
+        print 'Please enter an appropriate PATIENT_ID and an integer for dilation/erosion radius as inputs.'
         raise
 
-    # Write to CSV file
-    write_resected_electrodes(PATIENT_ID,dilate_radius)
+    # Generate list of resected electrodes and write to CSV file
+    write_resected_electrodes(patient_id, dilate_radius)
+
+    # Load resected electrodes
+    # Load electrodes to ignore
+    # Load ictal clips and get data as T x N for T = epoch_length (seconds) * fs
+    # For each clip, broken up in to epochs of length epoch_length
+        # Compute multiband connectivity
+        # Compute util.region_control == cres(t)
+        # Save with appropriate name
+    # Load
