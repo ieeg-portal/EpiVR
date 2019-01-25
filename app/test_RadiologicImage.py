3bcbc69d30da95363972ffb7592e9dab0d423c89
ThomasCampbellArnold <tcarnold.hub@gmail.com>
2019-01-25 17:40:55 -0500

    check all attributes of RadiologicImage class

--- app/test_RadiologicImage.py
+++ app/test_RadiologicImage.py
@@ -35,12 +35,17 @@ from RadiologicImage import RadiologicImage as ri
 import pytest
 
 
+# test creating class with valid input
 def test_ri_good_input():
     ex = ri("MRI", "T1", "20000123", ["x", "y", "z"], [2, 2, 2])
-    assert ex.contrast == 'T1'
+    assert ex.radiologic_type == "MRI"
+    assert ex.contrast == "T1"
+    assert ex.date_of_acquisition == "20000123"
+    assert ex.image_dimensions == ["x", "y", "z"]
+    assert ex.image_resolution == [2, 2, 2]
 
 
+# test that incorrect user type raises ValueError
 def test_ri_bad_type():
     with pytest.raises(ValueError, match=r'.* is not a valid image type..*'):
-        ex = ri("Garbage", "T1", "20000123", ["x", "y", "z"],
-                [2, 2, 2])
+        ex = ri("Garbage", "T1", "20000123", ["x", "y", "z"], [2, 2, 2])
