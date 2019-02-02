dd3c4dfdc0c9caebfa763774cd0de7975aca0f35
Lohith Kini <lkini86@gmail.com>
2019-02-02 17:42:09 -0500

    Added contact info

--- test/test_RadiologicImage.py
+++ test/test_RadiologicImage.py
@@ -2,6 +2,7 @@
 # Copyright (c) 2019 Center for Neuroengineering and Therapeutics.
 # Copyright (c) 2019 University of Pennsylvania.
 # License: MIT, see https://mit-license.org/
+# Contact: tcarnold@seas.upenn.edu
 
 """RadiologicImage class
 
