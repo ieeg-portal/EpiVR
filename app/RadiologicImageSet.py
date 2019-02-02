9528c65d313e890ce4c3067bf29d7c779aa2fc4d
Lohith Kini <lkini86@gmail.com>
2019-02-02 18:48:25 -0500

    creating initial RadiologicImageSet.py file

---
+++ app/RadiologicImageSet.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2019 Center for Neuroengineering and Therapeutics.
+# Copyright (c) 2019 University of Pennsylvania.
+# License: MIT, see https://mit-license.org/
+# Contact: tcarnold@seas.upenn.edu
+
