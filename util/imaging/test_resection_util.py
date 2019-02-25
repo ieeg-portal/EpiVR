e25468a2266def9188d64c9d62c63cc526d2cf03
Lohith Kini <lkini86@gmail.com>
2019-02-25 17:45:08 -0500

    fixed bug in unit test for resection_util

--- util/imaging/test_resection_util.py
+++ util/imaging/test_resection_util.py
@@ -7,4 +7,4 @@ import resection_util
 from app.RadiologicImageSet import RadiologicImageSet as ris
 
 result_ris = resection_util.load_image_manifest('HUP064')
-assert len(ris.radiology_images) == len(open(os.path.join(DATA_DIR, 'HUP064', 'image_manifest.csv'), 'r').readlines())
+assert len(result_ris.radiology_images) == len(open(os.path.join(DATA_DIR, 'HUP064', 'image_manifest.csv'), 'r').readlines())
