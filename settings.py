629614b27c333fbce4e5abcdf1eaf0addd493888
Lohith Kini <lkini86@gmail.com>
2019-02-25 17:37:30 -0500

    setup settings.py with approv env path

--- settings.py
+++ settings.py
@@ -6,7 +6,7 @@
 import os
 from dotenv import load_dotenv
 
-env_path = Path('.') / '.env'
+env_path = os.path.join(os.getcwd(), '.env')
 load_dotenv(dotenv_path=env_path, verbose=True)
 
 COMP_DIR = os.getenv("COMP_DIR")
