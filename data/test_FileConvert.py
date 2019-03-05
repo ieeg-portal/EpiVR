f149df791dc6f1c6b64712ab9d8f01cd615c7aef
ThomasCampbellArnold <tcarnold.hub@gmail.com>
2019-03-05 10:57:07 -0500

    added FileConvert and test_FileConvert

--- data/test_FileConvert.py
+++ data/test_FileConvert.py
@@ -5,15 +5,19 @@
 # Contact: tcarnold@seas.upenn.edu
 
 import FileConvert as fc
+import scipy.io
+import numpy as np
+import pytest
 
-def test_npz2mat(filename):
 
-    fc.mat2npz('test_mat2npz.mat')
-    data = np.load('test_mat2npz.npz')
+def test_npz2mat():
+
+    fc.npz2mat('test_npz2mat.npz')
+    data = scipy.io.loadmat('test_npz2mat.mat')
     assert data['A'][1][1] == 0.34011903728438
 
 
-def test_mat2npz(filename):
+def test_mat2npz():
 
     fc.mat2npz('test_mat2npz.mat')
     data = np.load('test_mat2npz.npz')
