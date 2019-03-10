ad7552ec743ee76576a61d3dcd4bcbce7050c053
Lohith Kini <lkini86@gmail.com>
2019-03-10 01:28:55 -0500

    added patient ID directory

--- util/event_manifest/test_create_event_manifest.py
+++ util/event_manifest/test_create_event_manifest.py
@@ -7,10 +7,11 @@ import create_event_manifest
 
 if not os.path.isdir('tmp'):
     os.makedirs('tmp')
+    os.makedirs('tmp/HUP116')
 create_event_manifest.create_event_manifest(
     r'/gdrive/public/USERS/lkini/3T_Subjects/HUP116/img',
     'tmp')
-csvfile = open("tmp/event_manifest.csv", "r")
+csvfile = open("tmp/HUP116/event_manifest.csv", "r")
 reader = csv.reader(csvfile)
 for row in reader:
     print(row)
