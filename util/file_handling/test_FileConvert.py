cfd2fde93ce2017acdced54d4bf78960132e3392
Lohith Kini <lkini86@gmail.com>
2019-03-09 23:49:27 -0500

    added cleaning up unit test results

--- util/file_handling/test_FileConvert.py
+++ util/file_handling/test_FileConvert.py
@@ -3,29 +3,28 @@
 # Copyright (c) 2019 University of Pennsylvania.
 # License: MIT, see https://mit-license.org/
 # Contact: tcarnold@seas.upenn.edu
-
-import FileConvert as fc
+import os
 import scipy.io
 import numpy as np
-import pytest
+import FileConvert as fc
 
 
 def test_npz2mat():
-
     fc.npz2mat('tests/data/test_npz2mat.npz')
     data = scipy.io.loadmat('tests/data/test_npz2mat.mat')
     assert data['A'][1][1] == 0.34011903728438
+    os.system('rm tests/data/test_npz2mat.mat')
 
 
 def test_mat2npz():
-
     fc.mat2npz('tests/data/test_mat2npz.mat')
     data = np.load('tests/data/test_mat2npz.npz')
     assert data['A'][1][1] == 0.34011903728438
+    os.system('rm tests/data/test_mat2npz.npz')
 
 
 def test_mat2npz_v73():
-
     fc.mat2npz_v73('tests/data/test_mat2npz_v73.mat')
     data = np.load('tests/data/test_mat2npz_v73.npz')
     assert data['A'][1][1] == 0.34011903728438
+    os.system('rm tests/data/test_mat2npz_v73.npz')
